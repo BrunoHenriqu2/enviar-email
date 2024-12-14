3dc33fe851b2a7610f987108338aff5744b50643
Bruno Henrique Hoffmann do Nascimento <brunohenriquehoffmann55@gmail.com>
2024-12-13 21:04:38 -0300

    Update

--- public/front-end.js
+++ public/front-end.js
@@ -1,6 +1,6 @@
 const button = document.body.querySelector("button#send")
 
 button.addEventListener("click", () => {
-    fetch("https://enviar-email-omega.vercel.app/")
+    fetch("/")
     .then(response => console.log(response))
 })

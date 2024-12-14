e7b44651543745d8934b8b5d4e742774eba10b2f
Bruno Henrique Hoffmann do Nascimento <brunohenriquehoffmann55@gmail.com>
2024-12-14 00:38:11 -0300

    Update

--- back/app.js
+++ back/app.js
@@ -1,12 +1,23 @@
-import sendEmail from  "./nodeMailer.js";
-import express from "express"
+// import sendEmail from  "./nodeMailer.js";
+// import express from "express"
 
-const expressApp = express()
+// const expressApp = express()
 
-expressApp.get("/info", (req, res) => {
-    res.send("Info sobre o projeto")
-})
+// expressApp.get("/info", (req, res) => {
+//     res.send("Info sobre o projeto")
+// })
 
-expressApp.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000!");
-})
+// expressApp.listen(3000, () => {
+//     console.log("Servidor rodando na porta 3000!");
+// })
+
+const express = require('express');
+const app = express();
+
+app.get('/api/getInfo', (req, res) => {
+    res.send('Info sobre o projeto');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000!');
+});
--- public/front-end.js
+++ public/front-end.js
@@ -1,8 +1,17 @@
-const button = document.body.querySelector("button#send")
+// const button = document.body.querySelector("button#send")
+
+// button.addEventListener("click", () => {
+//     fetch("/info")
+//         .then(response => response.text()) // Acessando o corpo da resposta como texto
+//         .then(data => console.log(data))
+//         .catch(error => console.error(error));
+// })
+
+const button = document.body.querySelector("button#send");
 
 button.addEventListener("click", () => {
     fetch("/info")
-        .then(response => response.text()) // Acessando o corpo da resposta como texto
+        .then(response => response.text())
         .then(data => console.log(data))
         .catch(error => console.error(error));
-})
+});

8ef616ce0357ea73af91a2a90cb1f1cadaedc2f0
Bruno Henrique Hoffmann do Nascimento <brunohenriquehoffmann55@gmail.com>
2024-12-14 00:16:16 -0300

    Update

--- public/front-end.js
+++ public/front-end.js
@@ -1,6 +1,6 @@
 const button = document.body.querySelector("button#send")
 
 button.addEventListener("click", () => {
-    fetch("/")
+    fetch("/info")
     .then(response => console.log(response))
 })

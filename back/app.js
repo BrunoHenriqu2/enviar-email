21fe1a4b13c80d5218dbaebc71c21288e7a6e2bf
Bruno Henrique Hoffmann do Nascimento <brunohenriquehoffmann55@gmail.com>
2024-12-14 00:24:52 -0300

    Update

--- back/app.js
+++ back/app.js
@@ -5,7 +5,6 @@ const expressApp = express()
 
 expressApp.get("/info", (req, res) => {
     res.send("Info sobre o projeto")
-    res.status(200)
 })
 
 expressApp.listen(3000, () => {
--- public/front-end.js
+++ public/front-end.js
@@ -2,5 +2,6 @@ const button = document.body.querySelector("button#send")
 
 button.addEventListener("click", () => {
     fetch("/info")
-    .then(response => console.log(response))
+    .then(response => console.log(response.text()))
+    .then(data => console.log(data))
 })

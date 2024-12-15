7c6c733d47e9c4902b46158f0bb84ce3a11f0bdf
Bruno Henrique Hoffmann do Nascimento <brunohenriquehoffmann55@gmail.com>
2024-12-14 23:17:11 -0300

    Update

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,26 +1,32 @@
-import nodemailer from "nodemailer"
-import dotenv from "dotenv"
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
-export default function sendEmail(req, res) {
-    dotenv.config()
+export default async function sendEmail(req, res) {
+    dotenv.config();
 
-    const transporter = nodemailer.createTransport({
-        service: process.env.SERVICE,
-        auth: {
-            user: process.env.EMAIL_FROM,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
+    try {
+        const transporter = nodemailer.createTransport({
+            service: process.env.SERVICE,
+            auth: {
+                user: process.env.EMAIL_FROM,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
 
-    const mailOptions = {
-        from: process.env.EMAIL_FROM,
-        to: process.env.EMAIL_TO,
-        subject: "Message title",
-        text: "Plaintext version of the message",
-        html: "<h1>HTML version of the message</h1>",
+        const mailOptions = {
+            from: process.env.EMAIL_FROM,
+            to: process.env.EMAIL_TO,
+            subject: "Message title",
+            text: "Plaintext version of the message",
+            html: "<h1>HTML version of the message</h1>",
+        };
+
+        // Aguarda o envio do e-mail
+        await transporter.sendMail(mailOptions);
+
+        res.status(200).json({ message: "Email enviado com sucesso!" });
+    } catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).json({ error: "falha no envio do email!." });
     }
-
-    transporter.sendMail(mailOptions)
-
-    res.status(200)
 }
